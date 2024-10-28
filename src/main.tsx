efb362d5e18292ee79aaea74f42ee280e4d7aeb6
Albert <pivnenko.albert@gmail.com>
2024-10-28 22:26:25 +0200

    added absolute path, restructer folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
-
-import "./index.css";
+import { App } from "@/app";
 
 const container = document.getElementById("root") as HTMLElement;
 
